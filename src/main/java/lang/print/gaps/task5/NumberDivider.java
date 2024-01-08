5775449537e31f1a3a941831b93adefc5bed7c46
andsps <spirkevichusandrei@mail.ru>
2024-01-08 16:52:58 +0300

    Third task

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         float result;
-        result = toBeDivided / 5;
+        result = toBeDivided / 5F;
         System.out.println(result);
     }
 }
